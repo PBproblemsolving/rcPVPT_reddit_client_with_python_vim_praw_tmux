77fe610e2d419cc4363205dd1fcb651cd8d74393
pbps <pbproblemsolving@protonmail.com>
2024-01-31 16:13:39 +0000

    removed file

--- credentials_example.py
+++
@@ -1,9 +0,0 @@
-import praw
-
-#used old.reddit.com to gain instant acces to api
-ruser = praw.Reddit(client_id = '',
-                     client_secret = '',
-                     username = '',
-                     password = '',
-                     user_agent = '')
-
