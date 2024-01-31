1320c6c0fcceb111168f93622f1c63e9788420f3
pbps <pbproblemsolving@protonmail.com>
2024-01-31 17:59:09 +0000

    redditmux

--- .redditmux.sh
+++ .redditmux.sh
@@ -4,10 +4,8 @@ tmux split-window -h -p 33
 cd reddit_client_with_python_vim_praw
 tmux split-window -v
 cd reddit_client_with_python_vim_praw
-vim out2
 tmux select-pane -t 0
 cd reddit_client_with_python_vim_praw
-vim output.txt
 tmux split-window -v
 tmux split-window -h
 cd reddit_client_with_python_vim_praw
