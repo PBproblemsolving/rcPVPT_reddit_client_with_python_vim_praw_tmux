912829de209ff2b11cc27b6dda46bc676e5b0d6f
pbps <pbproblemsolving@protonmail.com>
2024-01-31 15:33:18 +0000

    lost count

--- reader.py
+++ reader.py
@@ -4,69 +4,78 @@ import fire
 import datetime
 
 
-def stamptostring(timestamp):
+def _stamptostring(timestamp):
     return datetime.datetime.utcfromtimestamp(timestamp)
 
-def attr_transformer(attr_value, string_template="{}", lambdaf= lambda x: x):
+def _attr_transformer(attr_value, string_template="{}", lambdaf= lambda x: x):
     return string_template.format(lambdaf(attr_value))
 
-submission_attrs = ('author', 'created_utc', 'id', 'title', 'num_comments', 
+_submission_attrs = ('id', 'author', 'created_utc', 'title', 'num_comments', 
                     'score', 'upvote_ratio', 'spoiler') 
-default_transformers = {}
-submission_attrs_dict = {key: default_transformers for key in submission_attrs}
-submission_attrs_dict['id']={'string_template':"id: <<{}>>"}
-submission_attrs_dict['created_utc']={'lambdaf':lambda x: stamptostring(x)}
-submission_attrs_dict['spoiler']={'string_template':'spoiler: {}'}
-submission_attrs_dict['num_comments']={'string_template':"cmnts: {}"}
+_default_transformers = {}
+_submission_attrs_dict = {key: _default_transformers 
+                          for key in _submission_attrs}
+_submission_attrs_dict['id']={'string_template':"<<{}>>"}
+_submission_attrs_dict['created_utc']={'lambdaf':lambda x: _stamptostring(x)}
+_submission_attrs_dict['spoiler']={'string_template':'spoiler: {}'}
+_submission_attrs_dict['num_comments']={'string_template':"cmnts: {}"}
 
-def attrs_dict_factory(attrs_iter):
+def _attrs_dict_factory(attrs_iter):
     return {key: {} for key in attrs_iter}
 
-def subreddit_subs(sub_name, hot_new='hot', limit: int=20):
+def subreddit_submissions(sub_name, out_file='output.txt', 
+                   hot_new='hot', limit: int=20):
     if hot_new == 'new':    
         sub = ruser.subreddit(sub_name).new(limit=limit)
     if hot_new == 'hot':
         sub = ruser.subreddit(sub_name).hot(limit=limit)
-    with open('output.txt', 'w') as f:
+    with open(out_file, 'w') as f:
         for s in sub:
             s = ruser.submission(s)
             def line_to_write():
-                 for attr in submission_attrs:
-                    yield attr_transformer(getattr(s, attr, "-"),
-                                           **submission_attrs_dict.get(attr))
+                 for attr in _submission_attrs:
+                    yield _attr_transformer(getattr(s, attr, "-"),
+                                           **_submission_attrs_dict.get(attr))
 
             f.write("; ".join(line_to_write()))
             f.write("\n")
 
-comment_attrs = ('author', 'id', 'parent_id', 'body')
-comment_attrs_dict = attrs_dict_factory(comment_attrs)
+_comment_attrs = ('id', 'author', 'parent_id', 'body')
+_comment_attrs_dict = _attrs_dict_factory(_comment_attrs)
 
-def submission_coms(subs_id):
+def submission_coms(subs_id, out_file='output.txt'):
     c_submission = ruser.submission(subs_id)
     comments = c_submission.comments
     def iter_coms(comments, tabs):
         tabsstr = tabs*'\t'
         for c in comments:
             def line_to_write():
-                for attr in comment_attrs:
-                    yield tabsstr + attr_transformer(getattr(c, attr, "-"),
-                                           **comment_attrs_dict.get(attr))
-            try:
-                f.write("\n".join(line_to_write()))
-                f.write("\n---\n")
-            except AttributeError:
-                iter_coms(c.comments(), 0)
+                for attr in _comment_attrs:
+                    yield tabsstr + _attr_transformer(getattr(c, attr, "-"),
+                                           **_comment_attrs_dict.get(attr))
+            f.write("\n".join(line_to_write()))
+            f.write("\n---\n")
             try:
                 replies = c.replies
                 iter_coms(replies, tabs+1)
+            except AttributeError:
+                if tabs == 0:
+                    decision2 = input(
+                            "should load more comments? y for yes: ")
+                    if decision2 == 'y':
+                        iter_coms(c.comments(), 0)
+                    else:
+                        continue
+                else:
+                    iter_coms(c.comments(), tabs)
             except Exception as e:
                 print(e)
-    with open('output.txt', 'w') as f:
+    with open(out_file, 'w') as f:
         f.write(f"{c_submission.name}: \n \
                 {c_submission.selftext or c_submission.url}\n")
         iter_coms(comments, 0)
 
-def reply(to_whom):
+def _reply(to_whom):
     def inner(*args):
         reply_object, details = to_whom(*args)
         print('you are replying to:')
@@ -74,26 +83,29 @@ def reply(to_whom):
         print('with such content:')
         with open('input.txt', 'r') as f:
             content = f.read()
-            print(content)
-            decision = input("do you want to proceed? y for yes: ")
-            if decision == 'y':
-                answer = reply_object.reply(content)
-                print(f"your id {answer.id} and body: \n{answer.body}")
-            else:
-                print('aborted')
+        print(content)
+        decision = input("do you want to proceed? y for yes: ")
+        if decision == 'y':
+            answer = reply_object.reply(content)
+            print(f"your id {answer.id} and body: \n{answer.body}")
+        else:
+            print('aborted')
     return inner   
 
-@reply
+@_reply
 def reply_comment(comment_id):
     obj = ruser.comment(comment_id)
     return obj, f"{obj.id}, {obj.body[:30]}"
 
-@reply
+@_reply
 def reply_submission(submission_id):
     obj =  ruser.submission(submission_id)
     return obj, f"{obj.id}, {obj.title}"
 
-
+@_reply
+def reply_message(message_id):
+    obj = ruser.inbox.message(message_id)
+    return obj, f"{obj.id}, {obj.fullname} | {obj.body}"
 
 if __name__ == '__main__':
     fire.Fire()
